1bd6455dfb96c0932cfab463bf210a35ba904b4b
makar0nkin <ilya.makarenko2004@gmail.com>
2023-10-04 13:10:12 +0300

    feat: implement parantheses

--- main.py
+++ main.py
@@ -16,9 +16,41 @@ def calc_sub_expr(sub: tuple[str, str, str]) -> int | float:
             return n1 - n2
 
 def calculate(input: str) -> int | float:
+    def process_operations(matches: list[str]) -> int | float:
+        operations_order = ['**', '*', '/', '+', '-']
+
+        # handle parentheses
+        parentheses_counter: dict[str, list[int]] = {'left': [], 'right': []}
+        parentheses_pairs: list[tuple[int, int]] = []
+        for i, m in enumerate(matches):
+            if m == '(':
+                parentheses_counter['left'].append(i)
+            elif m == ')':
+                parentheses_counter['right'].append(i)
+            if all(parentheses_counter.values()) and \
+                    len(parentheses_counter['left']) == len(parentheses_counter['right']):
+                parentheses_pairs.append((parentheses_counter['left'][0], parentheses_counter['right'][-1]))
+                parentheses_counter = {'left': [], 'right': []}
+
+        # process parentheses
+        for start, end in reversed(parentheses_pairs):
+            tmp = process_operations(matches[start+1:end])
+            # print(f'del {matches[start:end+1]}')
+            del matches[start:end+1]
+            matches.insert(start, tmp)
+
+        # calculate
+        for o in operations_order:
+            for i, m in reversed(list(enumerate(matches))):
+                if m == o:
+                    sub_expr: tuple[str, str, str] = tuple(matches[i-1:i+2])
+                    del matches[i-1:i+2]
+                    matches.insert(i - 1, calc_sub_expr(sub_expr))
+        
+        return matches[0]
+    
     regex = r'((\*{1,2}|[+-\/]|^)-)?\d+|\*{1,2}|[+-\/\(\)]'
 
-    # input = input('Input expression:\t')  # '-22 + 0 / -2 - 100 * 10**-9 * -2'
     expr = input.replace(' ', '')
 
     matches = [*map(lambda m: m.group(), re.finditer(regex, expr, re.MULTILINE))]
@@ -32,19 +64,14 @@ def calculate(input: str) -> int | float:
         op, mod = bug.split('-')
         matches.insert(ind + count, op)
         matches.insert(ind + count + 1, f'-{mod}')
-
-    operations_order = ['**', '*', '/', '+', '-']
-
-    # calculate
-    for o in operations_order:
-        for i, m in reversed(list(enumerate(matches))):
-            if m == o:
-                sub_expr: tuple[str, str, str] = tuple(matches[i-1:i+2])
-                del matches[i-1:i+2]
-                matches.insert(i - 1, calc_sub_expr(sub_expr))
     
-    return matches[0]
+    return process_operations(matches)
     
-input = input('Input expression:\t')
-res = calculate(input)
-print(f'Expected:\t{eval(input)}', f'Actually:\t{res}', sep='\n')
+
+inputs = ['(6 / (5 - 2)) * (10 - 7)',
+          '((2 + 2) * 2)',
+          '((3 - 1) * (9 - 7)) / 4',
+          '(1 + 2 + 3 + 4 - 1) * (8 / (7 - (2 ** (5 - 1) - 7) / 3))']
+
+for inp in inputs:
+    assert calculate(inp) == eval(inp)
